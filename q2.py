776d4ebf6da61a4b06d4ac44ce21ecdc6ea48c02
pranavi jamdar <pranavijamdar39@gmail.com>
2026-07-08 10:37:23 +0530

    Add function to check if a number is even or odd

---
+++ q2.py
@@ -0,0 +1,15 @@
+def ChkNum(No):
+    if No % 2 == 0:
+     print("it is even number:")
+    else:
+     print("it is odd number")
+     
+def main():
+    print("enter the number number you want:")
+    No = int(input())
+    ChkNum(No)
+
+if __name__ == "__main__":
+   main()
+
+    
